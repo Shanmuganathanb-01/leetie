1c78cc7f5aaa180a47b8ad9a2be61b0f597b4f37
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-08-28 15:36:09 +0000

    leetie: Add 581. Shortest Unsorted Continuous Subarray [Medium] (java)

--- solutions/Medium/0581-shortest-unsorted-continuous-subarray/solution.java
+++ solutions/Medium/0581-shortest-unsorted-continuous-subarray/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Two Pointers, Stack, Greedy, Sorting, Monotonic Stack
 // Link     : https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42600000 (beats 0%)
+// Runtime  : 1 ms (beats 87%)
+// Memory   : 47228000 (beats 58%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
