71a8e9ecce5b4d68857dc777b1d72c068b1a285c
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-08-28 15:38:51 +0000

    leetie: Add 605. Can Place Flowers [Easy] (java)

--- solutions/Easy/0605-can-place-flowers/solution.java
+++ solutions/Easy/0605-can-place-flowers/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Greedy
 // Link     : https://leetcode.com/problems/can-place-flowers/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 41896000 (beats 0%)
+// Runtime  : 1 ms (beats 99%)
+// Memory   : 48032000 (beats 5%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
