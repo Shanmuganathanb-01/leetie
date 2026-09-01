51aeb84f1071cbe5fd0625c68f3fdb0b57f0e815
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-01 14:03:15 +0000

    leetie: Add 2626. Array Reduce Transformation [Easy] (javascript)

--- solutions/Easy/2626-array-reduce-transformation/solution.js
+++ solutions/Easy/2626-array-reduce-transformation/solution.js
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : N/A
 // Link     : https://leetcode.com/problems/array-reduce-transformation/
-// Runtime  : 45 ms (beats 0%)
-// Memory   : 52704000 (beats 0%)
+// Runtime  : 45 ms (beats 58%)
+// Memory   : 53788000 (beats 73%)
 // Language : javascript
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
