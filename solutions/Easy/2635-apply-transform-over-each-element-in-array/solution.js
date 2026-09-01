ebb04adffe0952af50c6475f7c2f944071a98794
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-01 14:08:11 +0000

    leetie: Add 2635. Apply Transform Over Each Element in Array [Easy] (javascript)

--- solutions/Easy/2635-apply-transform-over-each-element-in-array/solution.js
+++ solutions/Easy/2635-apply-transform-over-each-element-in-array/solution.js
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : N/A
 // Link     : https://leetcode.com/problems/apply-transform-over-each-element-in-array/
-// Runtime  : 48 ms (beats 0%)
-// Memory   : 52480000 (beats 0%)
+// Runtime  : 31 ms (beats 97%)
+// Memory   : 52896000 (beats 87%)
 // Language : javascript
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
