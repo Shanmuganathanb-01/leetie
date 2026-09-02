fafa99c9170020b4037a0c607e948109ad34d275
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-02 13:14:14 +0000

    leetie: Add 2704. To Be Or Not To Be [Easy] (javascript)

--- solutions/Easy/2704-to-be-or-not-to-be/solution.js
+++ solutions/Easy/2704-to-be-or-not-to-be/solution.js
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : N/A
 // Link     : https://leetcode.com/problems/to-be-or-not-to-be/
-// Runtime  : 57 ms (beats 0%)
-// Memory   : 54368000 (beats 0%)
+// Runtime  : 47 ms (beats 37%)
+// Memory   : 53784000 (beats 40%)
 // Language : javascript
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
