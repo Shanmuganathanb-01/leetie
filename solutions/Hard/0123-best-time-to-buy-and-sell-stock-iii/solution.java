9b81ea18f4262680bad36060df6a8d16d0d211b4
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-03 13:14:48 +0000

    leetie: Add 123. Best Time to Buy and Sell Stock III [Hard] (java)

--- solutions/Hard/0123-best-time-to-buy-and-sell-stock-iii/solution.java
+++ solutions/Hard/0123-best-time-to-buy-and-sell-stock-iii/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Array, Dynamic Programming
 // Link     : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 42448000 (beats 0%)
+// Memory   : 42464000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
@@ -12,12 +12,16 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
-            }
+        int t1Cost = Integer.MAX_VALUE, t2Cost = Integer.MAX_VALUE;
+        int t1Profit = 0, t2Profit = 0;
+
+        for (int price : prices) {
+            t1Cost = Math.min(t1Cost, price);
+            t1Profit = Math.max(t1Profit, price - t1Cost);
+            t2Cost = Math.min(t2Cost, price - t1Profit);
+            t2Profit = Math.max(t2Profit, price - t2Cost);
         }
-        return maxProfit;
+
+        return t2Profit;
     }
 }
