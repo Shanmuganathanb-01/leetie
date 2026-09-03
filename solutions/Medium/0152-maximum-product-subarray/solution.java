321252373b58d70e5446df64d2340844e3a9ad6d
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-03 14:18:19 +0000

    leetie: Add 152. Maximum Product Subarray [Medium] (java)

--- solutions/Medium/0152-maximum-product-subarray/solution.java
+++ solutions/Medium/0152-maximum-product-subarray/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming
 // Link     : https://leetcode.com/problems/maximum-product-subarray/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42724000 (beats 0%)
+// Runtime  : 2 ms (beats 72%)
+// Memory   : 47464000 (beats 16%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
