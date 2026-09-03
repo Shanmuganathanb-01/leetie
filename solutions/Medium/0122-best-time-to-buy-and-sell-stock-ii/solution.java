c32ddac958d7f65c6e498d0d95e20b2df760b28f
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-03 13:13:55 +0000

    leetie: Add 122. Best Time to Buy and Sell Stock II [Medium] (java)

--- solutions/Medium/0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ solutions/Medium/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Array, Dynamic Programming, Greedy
 // Link     : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 42316000 (beats 0%)
+// Memory   : 42456000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
@@ -12,17 +12,12 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            } else if (price - minPrice > maxProfit) {
-                maxProfit = price - minPrice;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
             }
         }
-        
         return maxProfit;
     }
 }
