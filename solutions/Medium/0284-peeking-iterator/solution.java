2aedc7d3bf5e0300074740b2a7d863e210b9f4fa
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-05 08:11:17 +0000

    leetie: Add 284. Peeking Iterator [Medium] (java)

---
+++ solutions/Medium/0284-peeking-iterator/solution.java
@@ -0,0 +1,39 @@
+// ──────────────────────────────────────────────────
+// Problem  : 284. Peeking Iterator
+// Difficulty: Medium
+// Tags     : Array, Design, Iterator
+// Link     : https://leetcode.com/problems/peeking-iterator/
+// Runtime  : 1 ms (beats 0%)
+// Memory   : 42876000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class PeekingIterator implements Iterator<Integer> {
+    private Iterator<Integer> iterator;
+    private Integer nextElement;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+        if (this.iterator.hasNext()) {
+            this.nextElement = this.iterator.next();
+        }
+    }
+
+    public Integer peek() {
+        return nextElement;
+    }
+
+    @Override
+    public Integer next() {
+        Integer res = nextElement;
+        nextElement = iterator.hasNext() ? iterator.next() : null;
+        return res;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextElement != null;
+    }
+}
