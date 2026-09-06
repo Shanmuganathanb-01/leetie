f4eefe2036e22b7b0d8268debe6211fcb76a6008
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-06 06:25:17 +0000

    leetie: Add 427. Construct Quad Tree [Medium] (java)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > *Automatically synced by [leetie](https://github.com/leetie/leetie).*
 
-## Progress Summary: 169 Solved
+## Progress Summary: 170 Solved
 
 | Slug | Problem | Difficulty | Language | Problem Link | Solution Code |
 |------|---------|-----------|----------|--------------|---------------|
@@ -42,6 +42,7 @@
 | compare-version-numbers | Compare Version Numbers | Medium | java | [Problem](https://leetcode.com/problems/compare-version-numbers/) | [Solution](./solutions/Medium/0165-compare-version-numbers/solution.java) |
 | construct-binary-tree-from-inorder-and-postorder-traversal | Construct Binary Tree from Inorder and Postorder Traversal | Medium | java | [Problem](https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/) | [Solution](./solutions/Medium/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.java) |
 | construct-binary-tree-from-preorder-and-inorder-traversal | Construct Binary Tree from Preorder and Inorder Traversal | Medium | java | [Problem](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/) | [Solution](./solutions/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java) |
+| construct-quad-tree | Construct Quad Tree | Medium | java | [Problem](https://leetcode.com/problems/construct-quad-tree/) | [Solution](./solutions/Medium/0427-construct-quad-tree/solution.java) |
 | construct-uniform-parity-array-i | Construct Uniform Parity Array I | Easy | java | [Problem](https://leetcode.com/problems/construct-uniform-parity-array-i/) | [Solution](./solutions/Easy/3875-construct-uniform-parity-array-i/solution.java) |
 | construct-uniform-parity-array-ii | Construct Uniform Parity Array II | Medium | java | [Problem](https://leetcode.com/problems/construct-uniform-parity-array-ii/) | [Solution](./solutions/Medium/3876-construct-uniform-parity-array-ii/solution.java) |
 | contains-duplicate | Contains Duplicate | Easy | java | [Problem](https://leetcode.com/problems/contains-duplicate/) | [Solution](./solutions/Easy/0217-contains-duplicate/solution.java) |
---
+++ solutions/Medium/0427-construct-quad-tree/solution.java
@@ -0,0 +1,84 @@
+// ──────────────────────────────────────────────────
+// Problem  : 427. Construct Quad Tree
+// Difficulty: Medium
+// Tags     : Array, Divide and Conquer, Tree, Matrix
+// Link     : https://leetcode.com/problems/construct-quad-tree/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42864000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return build(grid, 0, 0, grid.length);
+    }
+    
+    private Node build(int[][] grid, int r, int c, int length) {
+        if (isUniform(grid, r, c, length)) {
+            return new Node(grid[r][c] == 1, true);
+        }
+        
+        Node node = new Node(true, false);
+        int half = length / 2;
+        
+        node.topLeft = build(grid, r, c, half);
+        node.topRight = build(grid, r, c + half, half);
+        node.bottomLeft = build(grid, r + half, c, half);
+        node.bottomRight = build(grid, r + half, c + half, half);
+        
+        return node;
+    }
+    
+    private boolean isUniform(int[][] grid, int r, int c, int length) {
+        int val = grid[r][c];
+        for (int i = r; i < r + length; i++) {
+            for (int j = c; j < c + length; j++) {
+                if (grid[i][j] != val) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
