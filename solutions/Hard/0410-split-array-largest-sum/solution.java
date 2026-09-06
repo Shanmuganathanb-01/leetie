db7aa9c4c89e6a3422238b8184f8acdfd11078be
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-06 06:20:14 +0000

    leetie: Add 410. Split Array Largest Sum [Hard] (java)

--- solutions/Hard/0410-split-array-largest-sum/solution.java
+++ solutions/Hard/0410-split-array-largest-sum/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Binary Search, Dynamic Programming, Greedy, Prefix Sum
 // Link     : https://leetcode.com/problems/split-array-largest-sum/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42468000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43260000 (beats 17%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
