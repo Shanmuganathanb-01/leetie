2eba227c6081bfdfd89b656d95f2ace0a7070300
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-07 13:52:56 +0000

    leetie: Add 491. Non-decreasing Subsequences [Medium] (java)

--- solutions/Medium/0491-non-decreasing-subsequences/solution.java
+++ solutions/Medium/0491-non-decreasing-subsequences/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table, Backtracking, Bit Manipulation
 // Link     : https://leetcode.com/problems/non-decreasing-subsequences/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42472000 (beats 0%)
+// Runtime  : 19 ms (beats 16%)
+// Memory   : 53248000 (beats 36%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
