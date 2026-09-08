c9579c5627aca5a589bcaf426dbf617fe74a2d99
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-08 14:41:47 +0000

    leetie: Add 661. Image Smoother [Easy] (java)

--- solutions/Easy/0661-image-smoother/solution.java
+++ solutions/Easy/0661-image-smoother/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Matrix
 // Link     : https://leetcode.com/problems/image-smoother/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42616000 (beats 0%)
+// Runtime  : 5 ms (beats 84%)
+// Memory   : 48128000 (beats 6%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
