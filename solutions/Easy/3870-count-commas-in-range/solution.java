c2d14e52397b645a704990b7d9ee9cbd75a57ffe
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-08 14:11:51 +0000

    leetie: Add 3870. Count Commas in Range [Easy] (java)

--- solutions/Easy/3870-count-commas-in-range/solution.java
+++ solutions/Easy/3870-count-commas-in-range/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math
 // Link     : https://leetcode.com/problems/count-commas-in-range/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42092000 (beats 0%)
+// Runtime  : 1 ms (beats 84%)
+// Memory   : 42816000 (beats 8%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
