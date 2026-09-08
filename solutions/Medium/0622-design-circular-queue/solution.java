774faca6654fb05201cf824c02ab995710223563
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-08 14:20:11 +0000

    leetie: Add 622. Design Circular Queue [Medium] (java)

--- solutions/Medium/0622-design-circular-queue/solution.java
+++ solutions/Medium/0622-design-circular-queue/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Linked List, Design, Queue
 // Link     : https://leetcode.com/problems/design-circular-queue/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42808000 (beats 0%)
+// Runtime  : 4 ms (beats 100%)
+// Memory   : 46744000 (beats 15%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
