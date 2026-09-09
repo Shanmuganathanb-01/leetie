08d22e6a2d5dcc0f16812e569dcff82b86aaa473
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-09 12:07:24 +0000

    leetie: Add 3761. Minimum Absolute Distance Between Mirror Pairs [Medium] (java)

--- solutions/Medium/3761-minimum-absolute-distance-between-mirror-pairs/solution.java
+++ solutions/Medium/3761-minimum-absolute-distance-between-mirror-pairs/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Array, Hash Table, Math
 // Link     : https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 42408000 (beats 0%)
+// Memory   : 42488000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
