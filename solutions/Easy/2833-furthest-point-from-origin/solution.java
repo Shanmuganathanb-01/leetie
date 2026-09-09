4bcae5b1abc10147e700f6be3ba836d68c88defc
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-09 12:12:58 +0000

    leetie: Add 2833. Furthest Point From Origin [Easy] (java)

--- solutions/Easy/2833-furthest-point-from-origin/solution.java
+++ solutions/Easy/2833-furthest-point-from-origin/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : String, Counting
 // Link     : https://leetcode.com/problems/furthest-point-from-origin/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42584000 (beats 0%)
+// Runtime  : 1 ms (beats 100%)
+// Memory   : 43904000 (beats 28%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
