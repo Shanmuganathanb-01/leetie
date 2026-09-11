5d2a33773db7dd7b177d690d03673f5dd281a429
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-11 14:41:19 +0000

    leetie: Add 678. Valid Parenthesis String [Medium] (java)

--- solutions/Medium/0678-valid-parenthesis-string/solution.java
+++ solutions/Medium/0678-valid-parenthesis-string/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : String, Dynamic Programming, Stack, Greedy, Bracket Sequences
 // Link     : https://leetcode.com/problems/valid-parenthesis-string/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42696000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43044000 (beats 12%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
