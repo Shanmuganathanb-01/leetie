83972383e2432832b08517575b27629d194e1f57
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-11 14:48:02 +0000

    leetie: Add 763. Partition Labels [Medium] (java)

--- solutions/Medium/0763-partition-labels/solution.java
+++ solutions/Medium/0763-partition-labels/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Hash Table, Two Pointers, String, Greedy
 // Link     : https://leetcode.com/problems/partition-labels/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42556000 (beats 0%)
+// Runtime  : 5 ms (beats 81%)
+// Memory   : 43124000 (beats 95%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
