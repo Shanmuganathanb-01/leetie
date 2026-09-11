afff1f70e6be8e74e7a66938376f14edec02011e
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-11 14:35:26 +0000

    leetie: Add 517. Super Washing Machines [Hard] (java)

--- solutions/Hard/0517-super-washing-machines/solution.java
+++ solutions/Hard/0517-super-washing-machines/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Greedy
 // Link     : https://leetcode.com/problems/super-washing-machines/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42452000 (beats 0%)
+// Runtime  : 1 ms (beats 99%)
+// Memory   : 46380000 (beats 93%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
