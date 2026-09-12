365ca6f9f81e998d82b4852eda00f95d958b9b7d
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-12 08:08:06 +0000

    leetie: Add 290. Word Pattern [Easy] (java)

--- solutions/Easy/0290-word-pattern/solution.java
+++ solutions/Easy/0290-word-pattern/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String
 // Link     : https://leetcode.com/problems/word-pattern/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42780000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42668000 (beats 73%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
