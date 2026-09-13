f30b48b2991d1277f337d4686c30820596cd8b43
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-13 15:25:50 +0000

    leetie: Add 698. Partition to K Equal Sum Subsets [Medium] (java)

--- solutions/Medium/0698-partition-to-k-equal-sum-subsets/solution.java
+++ solutions/Medium/0698-partition-to-k-equal-sum-subsets/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Backtracking, Bit Manipulation, Memoization, Bitmask
 // Link     : https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42232000 (beats 0%)
+// Runtime  : 1549 ms (beats 18%)
+// Memory   : 42876000 (beats 89%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
