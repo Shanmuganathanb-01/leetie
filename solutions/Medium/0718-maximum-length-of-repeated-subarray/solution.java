70ec5da1698c080c49504c9a4ee5cafb8f2fec46
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-13 15:40:58 +0000

    leetie: Add 718. Maximum Length of Repeated Subarray [Medium] (java)

--- solutions/Medium/0718-maximum-length-of-repeated-subarray/solution.java
+++ solutions/Medium/0718-maximum-length-of-repeated-subarray/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Search, Dynamic Programming, Sliding Window, Rolling Hash, Hash Function
 // Link     : https://leetcode.com/problems/maximum-length-of-repeated-subarray/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42448000 (beats 0%)
+// Runtime  : 22 ms (beats 94%)
+// Memory   : 45276000 (beats 92%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
