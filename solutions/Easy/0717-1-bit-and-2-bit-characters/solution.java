e6fe95e1edd06f5288c0e70e73a27dd81bb8c2f2
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-13 15:39:32 +0000

    leetie: Add 717. 1-bit and 2-bit Characters [Easy] (java)

---
+++ solutions/Easy/0717-1-bit-and-2-bit-characters/solution.java
@@ -0,0 +1,28 @@
+// ──────────────────────────────────────────────────
+// Problem  : 717. 1-bit and 2-bit Characters
+// Difficulty: Easy
+// Tags     : Array
+// Link     : https://leetcode.com/problems/1-bit-and-2-bit-characters/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42248000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        int n = bits.length;
+        
+        while (i < n - 1) {
+            if (bits[i] == 1) {
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        
+        return i == n - 1;
+    }
+}
