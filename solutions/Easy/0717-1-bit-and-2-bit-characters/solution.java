d17446a3b3ae94ae0b720ee20aee3eb7780e3fb5
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-13 15:39:38 +0000

    leetie: Add 717. 1-bit and 2-bit Characters [Easy] (java)

--- solutions/Easy/0717-1-bit-and-2-bit-characters/solution.java
+++ solutions/Easy/0717-1-bit-and-2-bit-characters/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array
 // Link     : https://leetcode.com/problems/1-bit-and-2-bit-characters/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42248000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 44476000 (beats 47%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
