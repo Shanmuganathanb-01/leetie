aa439d0028168991cc8538f31cc3b0671faba57f
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-14 15:38:05 +0000

    leetie: Add 747. Largest Number At Least Twice of Others [Easy] (java)

--- solutions/Easy/0747-largest-number-at-least-twice-of-others/solution.java
+++ solutions/Easy/0747-largest-number-at-least-twice-of-others/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Sorting
 // Link     : https://leetcode.com/problems/largest-number-at-least-twice-of-others/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42804000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42812000 (beats 96%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
