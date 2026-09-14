aa088d2436efa3a2ad510e05cb25f8fc160bed29
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-14 15:39:09 +0000

    leetie: Add 748. Shortest Completing Word [Easy] (java)

--- solutions/Easy/0748-shortest-completing-word/solution.java
+++ solutions/Easy/0748-shortest-completing-word/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, String
 // Link     : https://leetcode.com/problems/shortest-completing-word/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42600000 (beats 0%)
+// Runtime  : 3 ms (beats 92%)
+// Memory   : 45920000 (beats 95%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
