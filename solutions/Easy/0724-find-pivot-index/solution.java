8caf74806ab81b7542d37aeff9b4d2fc6d23c0d8
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-14 15:25:23 +0000

    leetie: Add 724. Find Pivot Index [Easy] (java)

--- solutions/Easy/0724-find-pivot-index/solution.java
+++ solutions/Easy/0724-find-pivot-index/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Prefix Sum
 // Link     : https://leetcode.com/problems/find-pivot-index/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42440000 (beats 0%)
+// Runtime  : 1 ms (beats 96%)
+// Memory   : 47332000 (beats 60%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
