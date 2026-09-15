850c1a673467467cf6564c1dcf718b63f84a804c
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-15 15:22:04 +0000

    leetie: Add 1275. Find Winner on a Tic Tac Toe Game [Easy] (java)

--- solutions/Easy/1275-find-winner-on-a-tic-tac-toe-game/solution.java
+++ solutions/Easy/1275-find-winner-on-a-tic-tac-toe-game/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Matrix, Simulation
 // Link     : https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 43044000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43136000 (beats 25%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
