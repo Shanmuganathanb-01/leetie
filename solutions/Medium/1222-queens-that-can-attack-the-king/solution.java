a56af5593fb4565ea2b2ac952ce6a910e15ef062
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-15 15:19:38 +0000

    leetie: Add 1222. Queens That Can Attack the King [Medium] (java)

--- solutions/Medium/1222-queens-that-can-attack-the-king/solution.java
+++ solutions/Medium/1222-queens-that-can-attack-the-king/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Matrix, Simulation
 // Link     : https://leetcode.com/problems/queens-that-can-attack-the-king/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42580000 (beats 0%)
+// Runtime  : 1 ms (beats 81%)
+// Memory   : 44104000 (beats 72%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
