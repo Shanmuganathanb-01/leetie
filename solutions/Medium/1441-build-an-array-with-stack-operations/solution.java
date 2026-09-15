db58add724e3fa84785defc79388a06b38c8c025
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-15 15:27:06 +0000

    leetie: Add 1441. Build an Array With Stack Operations [Medium] (java)

--- solutions/Medium/1441-build-an-array-with-stack-operations/solution.java
+++ solutions/Medium/1441-build-an-array-with-stack-operations/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Stack, Simulation
 // Link     : https://leetcode.com/problems/build-an-array-with-stack-operations/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42992000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 44240000 (beats 53%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
