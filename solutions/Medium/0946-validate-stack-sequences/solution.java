2b660fea8bcb80da878dbab53aa2b2121dbba209
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-15 15:10:13 +0000

    leetie: Add 946. Validate Stack Sequences [Medium] (java)

--- solutions/Medium/0946-validate-stack-sequences/solution.java
+++ solutions/Medium/0946-validate-stack-sequences/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Stack, Simulation
 // Link     : https://leetcode.com/problems/validate-stack-sequences/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42760000 (beats 0%)
+// Runtime  : 2 ms (beats 89%)
+// Memory   : 45956000 (beats 95%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
