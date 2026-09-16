a4433515cbd47d332ea500b8697dda7c8302ac70
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-16 05:56:29 +0000

    leetie: Add 1583. Count Unhappy Friends [Medium] (java)

--- solutions/Medium/1583-count-unhappy-friends/solution.java
+++ solutions/Medium/1583-count-unhappy-friends/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Simulation
 // Link     : https://leetcode.com/problems/count-unhappy-friends/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42124000 (beats 0%)
+// Runtime  : 2 ms (beats 96%)
+// Memory   : 65728000 (beats 56%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
