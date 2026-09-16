309dc48feacdeb27c08c3f5ee5d5efb08247dd50
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-16 05:57:58 +0000

    leetie: Add 1603. Design Parking System [Easy] (java)

--- solutions/Easy/1603-design-parking-system/solution.java
+++ solutions/Easy/1603-design-parking-system/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Design, Simulation, Counting
 // Link     : https://leetcode.com/problems/design-parking-system/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42616000 (beats 0%)
+// Runtime  : 7 ms (beats 100%)
+// Memory   : 47268000 (beats 9%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
