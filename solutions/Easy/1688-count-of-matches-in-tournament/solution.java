28d1f64a5969f74f649d1bc26e7470de00e4217a
Shanmuganathanb-01 <shanmuganathanb01@users.noreply.github.com>
2026-09-16 06:00:52 +0000

    leetie: Add 1688. Count of Matches in Tournament [Easy] (java)

--- solutions/Easy/1688-count-of-matches-in-tournament/solution.java
+++ solutions/Easy/1688-count-of-matches-in-tournament/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Simulation
 // Link     : https://leetcode.com/problems/count-of-matches-in-tournament/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42028000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42048000 (beats 72%)
 // Language : java
 // Copyright: (c) 2026 Shanmuganathanb-01. All rights reserved.
 // Synced by: leetie
